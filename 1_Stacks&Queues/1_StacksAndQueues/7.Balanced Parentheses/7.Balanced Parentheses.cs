292703146cc4d2fd14127f355473bee342045e46
Stanislav Ivanov <stanislav.iv@gmail.com>
2018-01-28 01:41:40 +0200

    Stack&Queues

--- 1_Stacks&Queues/1_StacksAndQueues/7.Balanced Parentheses/7.Balanced Parentheses.cs
+++ 1_Stacks&Queues/1_StacksAndQueues/7.Balanced Parentheses/7.Balanced Parentheses.cs
@@ -10,7 +10,49 @@ namespace _7.Balanced_Parentheses
 	{
 		static void Main(string[] args)
 		{
+			char[] input = Console.ReadLine().ToCharArray();
 
+			if (input.Length % 2 != 0)
+			{
+				Console.WriteLine("NO");
+				Environment.Exit(0);
+			}
+
+			char[] opening = new[] { '(', '[', '{' };
+			char[] closing = new[] { ')', ']', '}' };
+
+			var stack = new Stack<char>();
+
+			foreach (var element in input)
+			{
+				if (opening.Contains(element))
+				{
+					stack.Push(element);
+				}
+				else if (closing.Contains(element))
+				{
+					var lastElement = stack.Pop();
+
+					int openingIndex = Array.IndexOf(opening, lastElement);
+					int closingIndex = Array.IndexOf(closing, element);
+
+
+					if (openingIndex != closingIndex)
+					{
+						Console.WriteLine("NO");
+						Environment.Exit(0);
+					}
+				}
+			}
+
+			if (stack.Any())
+			{
+				Console.WriteLine("NO");
+			}
+			else
+			{
+				Console.WriteLine("YES");
+			}
 		}
 	}
 }
